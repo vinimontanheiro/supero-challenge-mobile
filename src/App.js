2a56fcda3b10bcf2fc6048575482418129c6e9fa
Vinícius <mrvini@live.com>
2020-02-04 16:05:56 -0300

    Redux settings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import {Provider as ReduxProvider} from 'react-redux';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 import {I18nextProvider, useTranslation} from 'react-i18next';
 import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+import {persistor, store} from './services/redux/store';
 import i18next from './services/i18n/i18next';
 
 const App = () => {
   const {t} = useTranslation(`book`);
+
   return (
-    <I18nextProvider i18n={i18next}>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View>
-          <Text style={styles.sectionTitle}>{t(`title`)}</Text>
-        </View>
-      </SafeAreaView>
-    </I18nextProvider>
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <I18nextProvider i18n={i18next}>
+          <StatusBar barStyle="dark-content" />
+          <SafeAreaView>
+            <View>
+              <Text style={styles.sectionTitle}>{t(`title`)}</Text>
+            </View>
+          </SafeAreaView>
+        </I18nextProvider>
+      </PersistGate>
+    </ReduxProvider>
   );
 };
 
